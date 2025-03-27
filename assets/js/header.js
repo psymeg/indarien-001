609bf48270b10e646ff06658fd07087ec54c8bcd
psymeg <terryteo@gmail.com>
2025-03-27 14:05:52 +1300

    initial commit

---
+++ assets/js/header.js
@@ -0,0 +1,3 @@
+function createMenu(){
+  $("#inner").html('  <!-- Logo -->  <a href="index.html" class="logo"><span class="symbol"><img src="images/Indarien_Logo_Colour.png" alt="Indarien LTD" /></span><span class="title">Indarien</span></a>  <!-- Nav -->  <nav>  <ul>    <li><a href="#menu">Menu</a></li> <li><a href="emily.html">Emily and the Anteater</a></li> </ul>  </nav> ');
+}
